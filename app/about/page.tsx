283b9d75be6845f4d8947da2d7edb481b44b3650
bndnt <67003594+bndnt@users.noreply.github.com>
2025-11-27 02:07:37 -0500

    debug

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export const metadata: Metadata = {
+  title: 'About page',
+};
+const page = () => {
+  return <div>About</div>;
+};
+
+export default page;
---
+++ app/notes/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export const metadata: Metadata = {
+  title: 'Notes page',
+};
+const page = () => {
+  return <div>Notes</div>;
+};
+
+export default page;
